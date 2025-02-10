f823d778b78299155901905363ca7d043d9db13e
BANVIENCORP\tien.quach-dinh <tien.quach-dinh@banvien.com.vn>
2025-02-10 11:33:35 +0700

    change subset lusitana to import it from font/google.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] })
-export const lusitana = Inter({ subsets: ['lusitana'] })
+export const lusitana = Lusitana({ weight:'400' ,subsets: ['latin'] 
+});
